9957e8eaac4b356853dbc16c3a1286e0289ce69f
Longavtl <longavtl@gmail.com>
2025-03-25 11:25:03 +0700

    [longavtl] config build debug android

--- flutter_base_structure-main/lib/core/utils/device_info.dart
+++ flutter_base_structure-main/lib/core/utils/device_info.dart
@@ -1,24 +1,19 @@
-
 import 'dart:io';
-
 import 'package:device_info_plus/device_info_plus.dart';
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutter_keychain/flutter_keychain.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class DeviceInfo {
   static final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-  // static final storage = FlutterSecureStorage();
+  static final FlutterSecureStorage storage = const FlutterSecureStorage();
 
-  static Future<String?> get deviceId  async {
+  static Future<String?> get deviceId async {
     if (Platform.isIOS) {
-      // var deviceIdServer = await storage.read(key: 'device_id_ios_server');
-      var deviceIdServer = await FlutterKeychain.get(key: "device_id_ios_server");
-      if(deviceIdServer==null){
+      var deviceIdServer = await storage.read(key: 'device_id_ios_server');
+      if (deviceIdServer == null) {
         var iosDeviceInfo = await deviceInfo.iosInfo;
-        // await storage.write(key: 'device_id_ios_server', value: iosDeviceInfo.identifierForVendor);
-        await FlutterKeychain.put(key: "device_id_ios_server", value: iosDeviceInfo.identifierForVendor!);
-        deviceIdServer=iosDeviceInfo.identifierForVendor;
+        deviceIdServer = iosDeviceInfo.identifierForVendor;
+        await storage.write(key: 'device_id_ios_server', value: deviceIdServer);
       }
-
       return deviceIdServer;
     } else {
       var androidDeviceInfo = await deviceInfo.androidInfo;
@@ -26,7 +21,7 @@ class DeviceInfo {
     }
   }
 
-  static Future<String?> get deviceName  async {
+  static Future<String?> get deviceName async {
     if (Platform.isIOS) {
       var iosDeviceInfo = await deviceInfo.iosInfo;
       return iosDeviceInfo.model;
