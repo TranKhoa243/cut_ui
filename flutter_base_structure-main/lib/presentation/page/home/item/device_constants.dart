b363483a58ca07bdd63a81a03011257aa4c9b92e
Longavtl <longavtl@gmail.com>
2025-03-26 10:07:47 +0700

    [longavtl] botton navigator home

---
+++ flutter_base_structure-main/lib/presentation/page/home/item/device_constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeviceConstants {
+  const DeviceConstants._();
+
+  static const designSize = Size(designDeviceWidth, designDeviceHeight);
+
+  static const designDeviceWidth = 375.0;
+  static const designDeviceHeight = 812.0;
+
+  static const maxMobileWidth = 450;
+  static const maxTabletWidth = 900;
+
+  static const maxMobileWidthForDeviceType = 550;
+}
